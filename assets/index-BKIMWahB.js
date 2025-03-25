b462e313870335e3029d5c0c734d5f45ac4d5e92
Kos <k.svirski@gmail.com>
2025-03-25 22:26:33 +0100

    - improvements for write mode;

--- assets/index-BKIMWahB.js
+++ assets/index-BKIMWahB.js
@@ -1,4 +1,4 @@
-import{M as re,T as xt,R as N,C as x,a as ht,O as Z,b as X,E as j,c as Bo}from"./index-BNMFl0q0.js";import{q as Wo}from"./index-DilMv9Ng.js";/**
+import{M as re,T as xt,R as N,C as x,a as ht,O as Z,b as X,E as j,c as Bo}from"./index-BmYKWx4J.js";import{q as Wo}from"./index-DC90Uk5X.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
